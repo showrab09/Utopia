c6281fc41a00e81c87e79d0ee0c8a8cc81672f29
showrab09 <showrabbinhabib@gmail.com>
2022-12-23 01:24:38 +0530

    Products added

--- src/data.js
+++ src/data.js
@@ -1,53 +1,102 @@
 const list = [
-  {
-      id: 1,
-      title: 'Paper Bag',
-      img: 'https://m.media-amazon.com/images/I/71Dfv5djG5L._SX569_.jpg',
-      author: 'Paper Product',
-      amount: 1,
-      price: 10
-  },
-  {
-      id: 2,
-      title: 'Paper straw',
-      img: 'https://m.media-amazon.com/images/I/717ILnnHiZL._SX679_.jpg',
-      author: 'Paper Product',
-      amount: 1,
-      price: 60
-  },
-  {
-      id: 3,
-      title: 'Paper Glass',
-      img: 'https://m.media-amazon.com/images/I/71d2n2bkmMS._SY879_.jpg',
-      author: 'Paper Product',
-      amount: 1,
-      price: 300
-  },
-  {
-      id: 4,
-      title: 'Solar Panel Charging Cases',
-      img: 'https://m.media-amazon.com/images/I/61qJFhThURL._SX466_.jpg',
-      author: 'Green Product',
-      amount: 1,
-      price: 9000
-  },
-  {
-      id: 5,
-      title: 'Jute bag',
-      img: 'https://m.media-amazon.com/images/I/618H2xRJuHL._UY625_.jpg',
-      author: 'Jute Product',
-      amount: 1,
+    {
+        id: 1,
+        title: 'Paper Bag',
+        img: 'https://m.media-amazon.com/images/I/71Dfv5djG5L._SX569_.jpg',
+        author: 'Paper Product',
+        amount: 1,
+        price: 10
+    },
+    {
+        id: 2,
+        title: 'Paper straw',
+        img: 'https://m.media-amazon.com/images/I/717ILnnHiZL._SX679_.jpg',
+        author: 'Paper Product',
+        amount: 1,
+        price: 60
+    },
+    {
+        id: 3,
+        title: 'Paper Glass',
+        img: 'https://m.media-amazon.com/images/I/71d2n2bkmMS._SY879_.jpg',
+        author: 'Paper Product',
+        amount: 1,
+        price: 300
+    },
+    {
+        id: 4,
+        title: 'Solar Panel Charging Cases',
+        img: 'https://m.media-amazon.com/images/I/61qJFhThURL._SX466_.jpg',
+        author: 'Green Product',
+        amount: 1,
+        price: 9000
+    },
+    {
+        id: 5,
+        title: 'Jute bag',
+        img: 'https://m.media-amazon.com/images/I/618H2xRJuHL._UY625_.jpg',
+        author: 'Jute Product',
+        amount: 1,
 
-      price: 200
-  },
-  {
-      id: 6,
-      title: 'Plants',
-      img: 'https://m.media-amazon.com/images/I/41hI3-MepNL._SX300_SY300_QL70_FMwebp_.jpg',
-      author: 'Home Decoration',
-      amount: 1,
-      price: 500
-  }
+        price: 200
+    },
+    {
+        id: 6,
+        title: 'Plants',
+        img: 'https://m.media-amazon.com/images/I/41hI3-MepNL._SX300_SY300_QL70_FMwebp_.jpg',
+        author: 'Home Decoration',
+        amount: 1,
+        price: 500
+    },
+    {
+        id: 7,
+        title: 'Paper Bag',
+        img: 'https://m.media-amazon.com/images/I/81YU1zBZXZL._SX679_.jpg',
+        author: 'paper products',
+        amount: 1,
+        price: 50
+    },
+    {
+        id: 8,
+        title: 'Plants',
+        img: 'https://m.media-amazon.com/images/I/41I0iyhYjkL.jpg',
+        author: 'Home Decoration',
+        amount: 1,
+        price: 220
+    },
+    {
+        id: 9,
+        title: 'Paper plate',
+        img: 'https://m.media-amazon.com/images/I/619s+1mqbKL._SX569_.jpg',
+        author: 'paper products',
+        amount: 1,
+        price: 50
+    },
+    {
+        id: 10,
+        title: 'Bamboo Bowl',
+        img: 'https://m.media-amazon.com/images/I/61aGteF7lpL._SX569_.jpg',
+        author: 'Bamboo product',
+        amount: 1,
+        price: 59
+    },
+    {
+        id: 11,
+        title: 'Bamboo Towel',
+        img: 'https://m.media-amazon.com/images/I/81TvFkacHuS._SX569_.jpg',
+        author: 'Bamboo product',
+        amount: 1,
+        price: 400
+    }
+    , {
+        id: 12,
+        title: 'Bamboo Dustbin',
+        img: 'https://m.media-amazon.com/images/I/71414rrtQVL._SX425_.jpg',
+        author: 'Bamboo product',
+        amount: 1,
+        price: 300
+    }
+
 ];
 
 export default list;
